e0885e4aec8b098171102b88863e12aea2a7a366
NTTKDeveloper <nttkct2020@gmail.com>
2023-02-08 20:09:01 +0700

    Upgrade code

--- TestTCP.js
+++ TestTCP.js
@@ -8,13 +8,18 @@ const server = http.createServer(express);
 const wss = new WebSocket.Server({ server })
 
 wss.on('connection', function connection(ws, red) {
-  ws.on('message', function incoming(data_received) {
-    let now = new Date ();
-    writeLog_Msg(data_received, red, now);
-    data_send = "data_send";
-    ws.send(data_send.toString());
-  })
+  console.log("Client " + red.socket.address().address + ':' 
+                        + red.socket.address().port + " connected");
+  ws.on("close", () => console.log('Client '+ red.socket.address().address + ':' 
+                        + red.socket.address().port + ' disconnected'));
+  // ws.on('message', function incoming(data_received) {
+  //   let now = new Date ();
+  //   writeLog_Msg(data_received, red, now);
+  //   data_send = "data_send";
+  //   ws.send(data_send.toString());
+  // })
 })
+
 
 server.listen(port, function() {
   console.log(`Server is listening on ${port}!`)
@@ -22,8 +27,15 @@ server.listen(port, function() {
 
 //Hàm khi lại những messgage của client
 function writeLog_Msg(msg, red, now){
-    console.log('[ ' + now + ' ]' + ' Data received to ' 
-                + red.socket.address().address + ':' 
-                + red.socket.address().port + ' '  
-                + ' Msg: ' + msg);
+    mLog = '[ ' + now + ' ]' + ' Data received to ' 
+    + red.socket.address().address + ':' 
+    + red.socket.address().port + ' '  
+    + ' Msg: ' + msg;
+    console.log(mLog);
+    //Ghi vào file Logs
+    // fs.appendFile('./Logs/Mlog.txt', mLog.toString(),function(err, data){
+    //   if(err) throw err;
+    // });
 }
+
+ 
